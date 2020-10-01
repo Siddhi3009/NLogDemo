e84f5819318571538283ea48430fb8d18e1e04e3
Administrator <siddhiseth3009@gmail.com>
2020-10-01 11:05:47 +0530

    [Siddhi]Add Logger file added

--- NLogDemo/Program.cs
+++ NLogDemo/Program.cs
@@ -6,7 +6,11 @@ namespace NLogDemo
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Addition Problem \nEnter a & b values");
+            int a = int.Parse(Console.ReadLine());
+            int b = int.Parse(Console.ReadLine());
+            AddNumber add = new AddNumber();
+            Console.WriteLine("Result : " + add.Sum(a,b));
         }
     }
 }
